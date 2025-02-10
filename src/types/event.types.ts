faf5da2207bea03cf9142700df28219ad0513f8f
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2025-02-10 21:23:53 +0700

    Refactor event types and interface; add event type enumeration and make fields mandatory

--- src/types/event.types.ts
+++ src/types/event.types.ts
@@ -1,14 +1,26 @@
+export type IEventType =
+  | "MUSIC_CONCERT"
+  | "CULTURAL_ARTS"
+  | "TRAVEL"
+  | "WORKSHOP"
+  | "MOVIE"
+  | "TOUR"
+  | "SPORTS"
+  | "NEWS"
+  | "OTHER";
+
 export interface IEvent {
+  type: IEventType;
   _id: string;
   name: string;
-  imageThumbnail?: string;
+  imageThumbnail: string;
   time: string;
   location: string;
-  description?: string;
+  description: string;
   capacity: number;
   ticketsAvailable: number;
   priceTicket: number;
+  __v: number;
   createdAt: string;
   updatedAt: string;
-  __v: number;
 }
